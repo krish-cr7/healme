ec7d43cf87a61db01639d9253cf6755b5eae24b1
Devkrishna710 <krishnakritipro@gmail.com>
2024-04-12 01:42:53 +0530

    changed the button link

--- app.py
+++ app.py
@@ -99,5 +99,5 @@ def schedule():
 
     return render_template('schedule_meeting.html')
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
