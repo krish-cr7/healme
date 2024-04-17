5eabc3ff904afc693f2b41731662f498f415ea85
Devkrishna710 <krishnakritipro@gmail.com>
2024-04-17 19:14:53 +0530

    new code

--- app.py
+++ app.py
@@ -25,6 +25,11 @@ google = oauth.remote_app(
 
 # Razorpay Configuration
 razorpay_client = razorpay.Client(auth=(os.environ.get('RAZORPAY_KEY_ID'), os.environ.get('RAZORPAY_KEY_SECRET')))
+@app.before_first_request
+def before_first_request():
+
+    session['screenshot_uploaded'] = False
+
 
 @app.route('/')
 def landing_page():
@@ -37,7 +42,9 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('google_token', None)
+    session.pop('screenshot_uploaded', None)
     return redirect(url_for('landing_page'))
+
 
 @app.route('/authorized')
 def authorized():
@@ -56,32 +63,40 @@ def get_google_oauth_token():
 
 @app.route('/pay', methods=["GET", "POST"])
 def pay():
-    if request.form.get("amount") != "":
-        # amount=request.form.get("amt")
-        razorpay_key = os.environ.get('RAZORPAY_KEY_ID')
-        amount = 50000
-        data = { "amount": 50000, "currency": "INR", "receipt": "order_rcptid_11" }
-        payment = razorpay_client.order.create(data=data)
-        pdata=[amount, payment["id"]]
-        return render_template("index.html", pdata=pdata, razorpay_key=razorpay_key)
-    return redirect("/form")
+    if 'google_token' not in session:
+        return redirect(url_for('login'))
+    return render_template("index.html")
+    # return redirect("/form")
 
-@app.route('/success', methods=["POST"])
-def success():
-    pid = request.form.get("razorpay_payment_id")
-    ordid = request.form.get("razorpay_order_id")
-    sign = request.form.get("razorpay_signature")
-    print(f"The payment id : {pid}, order id : {ordid} and signature : {sign}")
-    params = {
-        'razorpay_order_id': ordid,
-        'razorpay_payment_id': pid,
-        'razorpay_signature': sign
-    }
-    final = razorpay_client.utility.verify_payment_signature(params)
-    if final:
-        session['payment_success'] = True
-        return redirect("/schedule")
-    return "Something Went Wrong Please Try Again"
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'google_token' not in session:
+        return redirect(url_for('login'))
+    if 'file' not in request.files:
+        return 'No file part'
+
+    # Set the screenshot uploaded status to True
+    session['screenshot_uploaded'] = True
+
+    return redirect(url_for('schedule'))
+
+
+# @app.route('/success', methods=["POST"])
+# def success():
+#     pid = request.form.get("razorpay_payment_id")
+#     ordid = request.form.get("razorpay_order_id")s
+#     sign = request.form.get("razorpay_signature")
+#     print(f"The payment id : {pid}, order id : {ordid} and signature : {sign}")
+#     params = {
+#         'razorpay_order_id': ordid,
+#         'razorpay_payment_id': pid,
+#         'razorpay_signature': sign
+#     }
+#     final = razorpay_client.utility.verify_payment_signature(params)
+#     if final:
+#         session['payment_success'] = True
+#         return redirect("/schedule")
+#     return "Something Went Wrong Please Try Again"
 
 @app.route('/form')
 def form():
@@ -93,11 +108,9 @@ def form():
 def schedule():
     if 'google_token' not in session:
         return redirect(url_for('login'))
+    if session.get('screenshot_uploaded') != True:
+        return "Unauthorized: Please upload screenshot first", 401
+    return render_template("schedule_meeting.html")
 
-    if session.get('payment_success') != True:
-        return "Unauthorized", 401
-
-    return render_template('schedule_meeting.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
